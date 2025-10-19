8341f38d5d0471c001df96256130aa805dff4a67
Michael Eaton <lammara96@gmail.com>
2025-10-19 00:31:08 -0400

    svc

--- src/shared_util/baseline.py
+++ src/shared_util/baseline.py
@@ -1,16 +1,21 @@
 from sklearn.metrics import make_scorer, roc_auc_score
 from imblearn.pipeline import Pipeline
-from shared_util.utils import make_cv
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, StratifiedKFold
 
-
+def make_cv():
+    return StratifiedKFold(
+    n_splits=10,
+    shuffle=True,
+    random_state=42
+    )
 
 def get_baseline_score(
         model,
         sampling_tech,
         X_train,
         y_train,
-        scorer=roc_auc_score
+        scorer=roc_auc_score,
+        scaler=None
         ) -> float:
     """
     Calculates a baseline score using the designated scorer function, default roc_auc
@@ -20,21 +25,32 @@ def get_baseline_score(
     Returns the mean score between folds
     """
     cv = make_cv()
-    pipe = Pipeline([
+
+    if scaler != None:
+       pipe = Pipeline([
+        ('sampler', sampling_tech),
+        ('scaler', scaler),
+        ('model', model)
+    ]) 
+    else:
+        pipe = Pipeline([
         ('sampler', sampling_tech),
         ('model', model)
-    ])
+    ]) 
+
+    
 
     scores = cross_val_score(
         pipe,
         X_train,
         y_train,
         cv = cv,
+        n_jobs=-1,
         scoring=make_scorer(scorer)
     )
 
-    print(f'{scorer} score per fold: ', scores)
-    print(f'mean {scorer} score: ', scores.mean())
+    print(f'{scorer.__name__} score per fold: ', scores)
+    print(f'mean {scorer.__name__} score: ', scores.mean())
     return scores.mean()
 
 
