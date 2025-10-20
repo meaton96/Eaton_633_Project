68fc818791f373573b35ee3c6544e63fced49671
Michael Eaton <lammara96@gmail.com>
2025-10-20 16:59:02 -0400

    fix split bug

--- src/shared_util/split.py
+++ src/shared_util/split.py
@@ -1,14 +1,14 @@
-# split initial train, validate, test
-import pandas as pd
 from sklearn.model_selection import train_test_split
 
-def train_val_split(df, target_col = 'target', test_size = 0.15, val_size = 0.15):
+def train_val_test_split(df, target_col = 'target', train_size = 0.7):
     X_train, X_, y_train, y_ = train_test_split(
-        df.drop(columns=['target']), 
-        df['target'], 
-        test_size=(test_size + val_size), 
-        stratify=df['target']
+        df.drop(columns=[target_col]), 
+        df[target_col], 
+        test_size=1-train_size, 
+        stratify=df[target_col]
         )
     
-    X_validate, X_test, y_validate, y_test = train_test_split(X_, y_, test_size=(test_size + val_size) / test_size, stratify=y_)
+    X_validate, X_test, y_validate, y_test = train_test_split(X_, y_, test_size=0.5, stratify=y_)
+
+    return X_train, X_validate, X_test, y_train, y_validate, y_test
 
